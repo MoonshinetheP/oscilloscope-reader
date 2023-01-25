41664a7adc4a3cf476e9760b359978c31315d4f4
Steven Linfield <S.Linfield@outlook.com>
2023-01-25 15:36:48 +0000

    Edited the class argument errors

--- src/oscilloscopereader/simulations.py
+++ src/oscilloscopereader/simulations.py
@@ -4,17 +4,86 @@ import numpy as np
 class Capacitance:
     '''Simulates the charging of a double layer when using CV or CSV \n 
     Also takes input parameters and uses them to create potential waveforms for CV and CSV'''
-    def __init__(self, Eini = 0.0, Eupp = 0.5, Elow = 0.0, dE = 0.002 , sr = 0.2, ns = 1, Cd = 0.000050, Ru = 500, sp = 1000):
-        
-        self.Eini = Eini        # Start potential
-        self.Eupp = Eupp        # Upper vertex potential
-        self.Elow = Elow        # Lower vertex potential
-        self.dE = dE        # Step size
-        self.sr = sr        # Scan rate
-        self.ns = ns        # Number of scans
-        self.Cd = Cd        # Double layer capacitance
-        self.Ru = Ru        # Uncompensated resistance
-        self.sp = sp        # Number of points in a step
+    def __init__(self, Eini = 0.0, Eupp = 0.5, Elow = -0.5, dE = 0.001 , sr = 0.1, ns = 1, Cd = 0.000050, Ru = 500, sp = 1000):
+        '''Define the parameters of the simulation, checks for errors, and makes a potential waveform to be used by other functions'''
+        self.Eini = Eini        # Start potential in V
+        self.Eupp = Eupp        # Upper vertex potential in V
+        self.Elow = Elow        # Lower vertex potential in V
+        self.dE = dE        # Step size in V
+        self.sr = sr        # Scan rate in V/s
+        self.ns = ns        # Number of scans (no unit)
+        self.Cd = Cd        # Double layer capacitance in F
+        self.Ru = Ru        # Uncompensated resistance in Ohms
+        self.sp = sp        # Number of data points in a step (no unit)
+
+
+        '''Datatype errors'''
+        if isinstance(self.Eini, (float, int)) is False:
+            print('\n' + 'An invalid datatype was used for the start potential. Enter either a float or an integer.' + '\n')
+            sys.exit()
+        if isinstance(self.Eupp, (float, int)) is False:
+            print('\n' + 'An invalid datatype was used for the upper vertex potential. Enter either a float or an integer.' + '\n')
+            sys.exit()        
+        if isinstance(self.Elow, (float, int)) is False:
+            print('\n' + 'An invalid datatype was used for the lower vertex potential. Enter either a float or an integer.' + '\n')
+            sys.exit()        
+        if isinstance(self.dE, (float, int)) is False:
+            print('\n' + 'An invalid datatype was used for the step potential. Enter either a float or an integer.' + '\n')
+            sys.exit()
+        if isinstance(self.sr, (float, int)) is False:
+            print('\n' + 'An invalid datatype was used for the scan rate. Enter either a float or an integer.' + '\n')
+            sys.exit()
+        if isinstance(self.ns, (int)) is False:
+            print('\n' + 'An invalid datatype was used for the number of scans. Enter an integer.' + '\n')
+            sys.exit()
+        if isinstance(self.Cd, (float, int)) is False:
+            print('\n' + 'An invalid datatype was used for the double layer capacitance. Enter either a float or an integer.' + '\n')
+            sys.exit()
+        if isinstance(self.Ru, (float, int)) is False:
+            print('\n' + 'An invalid datatype was used for the uncompensated resistance. Enter either a float or an integer.' + '\n')
+            sys.exit()
+        if isinstance(self.sp, (int)) is False:
+            print('\n' + 'An invalid datatype was used for the number of data points in a step. Enter an integer.' + '\n')
+            sys.exit()
+
+
+        '''Data value errors'''
+        if self.Eupp == self.Elow:
+            print('\n' + 'Upper and lower vertex potentials must be different values' + '\n')
+            sys.exit()
+        if self.Eupp < self.Elow:
+            print('\n' + 'Upper vertex potential must be greater than lower vertex potential' + '\n')
+            sys.exit()  
+        if self.Eini < self.Elow:
+            print('\n' + 'Start potential must be higher than or equal to the lower vertex potential' + '\n')
+            sys.exit()
+        if self.Eini > self.Eupp:
+            print('\n' + 'Start potential must be lower than or equal to the upper vertex potential' + '\n')
+            sys.exit()
+        if self.dE == 0:
+            print('\n' + 'Step potential must be a non-zero value' + '\n')
+            sys.exit()   
+        if self.Eini == self.Elow and self.dE < 0:
+            print('\n' + 'Step potential must be a positive value for a positive scan direction' + '\n')
+            sys.exit()
+        if self.Eini == self.Eupp and self.dE > 0:
+            print('\n' + 'Step potential must be a negative value for a negative scan direction' + '\n')
+            sys.exit()
+        if self.sr <= 0:
+            print('\n' + 'Scan rate must be a positive non-zero value' + '\n')
+            sys.exit()
+        if self.ns <=0:
+            print('\n' + 'Number of scans must be a positive non-zero value' + '\n')
+            sys.exit()
+        if self.Cd <= 0:
+            print('\n' + 'Double layer capacitance must be a positive non-zero value' + '\n')
+            sys.exit()
+        if self.Ru <= 0:
+            print('\n' + 'Uncompensated resistance must be a positive non-zero value' + '\n')
+            sys.exit()
+        if self.sp <= 0:
+            print('\n' + 'Number of data points in a step must be a positive non-zero value' + '\n')
+            sys.exit()
 
 
         if self.Eini == self.Elow and self.dE > 0:          
@@ -40,7 +109,25 @@ class Capacitance:
         
 
         if self.Eini == self.Eupp and self.dE < 0:
-            pass
+            self.segments = 2 * self.ns          # Number of segments expected          
+            self.window = self.Eupp - self.Eini         # Potential window of each segment
+            self.dp = int(self.window/self.dE)       # Number of data points in each potential window
+            
+            self.time = np.array([])
+            for ix in range(0, self.segments):
+                self.time = np.append(self.time, np.round(np.linspace(0, (self.window - self.dE)/self.sr, self.dp), decimals = 3))
+            
+            self.sweep = np.array([])
+            for iy in range(0, self.ns):
+                self.sweep = np.append(self.sweep, np.round(np.linspace(self.Eini, self.Eupp - self.dE, self.dp), decimals = 4))
+                self.sweep = np.append(self.sweep, np.round(np.linspace(self.Eupp, self.Eini + self.dE, self.dp), decimals = 4))
+        
+            self.step = np.array([])
+            for iz in range(0, self.segments * self.dp):
+                try:
+                    self.step = np.append(self.step, np.round(np.linspace(self.sweep[iz], self.sweep[iz + 1] - self.dE/self.sp, self.sp), decimals = 7))
+                except:
+                    self.step = np.append(self.step, np.round(np.linspace(self.sweep[iz], self.sweep[-1], self.sp + 1), decimals = 7))
 
 
         if self.Eini > self.Elow and self.Eini < Eupp:
@@ -65,19 +152,7 @@ class Capacitance:
                 self.sweep = np.append(self.sweep, np.round(np.linspace(self.Elow, self.Eini - self.dE, self.lowdp), decimals = 4))
             
 
-        '''Errors'''
-        if self.Eini < self.Elow:
-            print('\n' + 'Initial potential must be higher than the lower vertex potential' + '\n')
-            sys.exit()
-        if self.Eini > self.Eupp:
-            print('\n' + 'Initial potential must be lower than the upper vertex potential' + '\n')
-            sys.exit()
-        if self.Eini == self.Elow and self.dE < 0:
-            print('\n' + 'Step potential must be positive for a positive scan direction' + '\n')
-            sys.exit()
-        if self.Eini == self.Eupp and self.dE > 0:
-            print('\n' + 'Step potential must be negative for a negative scan direction' + '\n')
-            sys.exit()
+
 
     def temp(self):
         return self.sweep 
