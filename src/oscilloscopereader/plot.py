f8d9a4b4e5455ff341e510f4c58fa0c131cb25fa
Steven Linfield <68279135+MoonshinetheP@users.noreply.github.com>
2023-11-03 16:23:23 +0100

    Edited for remote control

--- src/oscilloscopereader/plot.py
+++ src/oscilloscopereader/plot.py
@@ -1,3 +1,46 @@
+'''
+===================================================================================================
+Copyright (C) 2023 Steven Linfield
+
+This file is part of the oscilloscope-reader package. This package is free software: you can 
+redistribute it and/or modify it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or (at your option) any later 
+version. This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details. You should have received a copy of the GNU General 
+Public License along with oscilloscope-reader. If not, see https://www.gnu.org/licenses/
+===================================================================================================
+
+Package title:      oscilloscope-reader
+Repository:         https://github.com/MoonshinetheP/oscilloscope-reader
+Date of creation:   22/10/2022
+Main author:        Steven Linfield (MoonshinetheP)
+Collaborators:      None
+Acknowledgements:   None
+
+Filename:           plot.py
+
+===================================================================================================
+
+Description:
+
+ 
+
+===================================================================================================
+
+How to use this file:
+    
+
+
+===================================================================================================
+
+Note:
+
+
+
+===================================================================================================
+'''
+
 import sys
 import os
 from errno import EEXIST
@@ -9,9 +52,51 @@ import matplotlib.pyplot as plt
 import waveforms as wf
 import simulations as sim
 
+class Plotter:
 
-if __name__ == '__main__':
-    
+    def __init__(self, shape, instance, display = True, save = True):
+
+        self.shape = shape
+        self.instance = instance
+        self.display = display
+        self.save = save
+
+
+        '''PLOT GENERATION'''
+        fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12, 5))
+        left, = ax1.plot(self.shape.tWF, self.shape.EWF, linewidth = 1, linestyle = '-', color = 'blue', marker = None, label = None, visible = True)
+        right, = ax2.plot(self.instance.EPLOT, self.instance.i[:self.instance.EPLOT.size], linewidth = 1, linestyle = '-', color = 'red', marker = None, label = None, visible = True)
+        
+
+        '''PLOT SETTINGS'''
+        ax1.set_xlim(np.amin(self.shape.tWF) - (0.1 * (np.amax(self.shape.tWF) - np.amin(self.shape.tWF))), np.amax(self.shape.tWF) + (0.1 * (np.amax(self.shape.tWF) - np.amin(self.shape.tWF))))
+        ax1.set_ylim(np.amin(self.shape.EWF) - (0.1 * (np.amax(self.shape.EWF) - np.amin(self.shape.EWF))), np.amax(self.shape.EWF) + (0.1 * (np.amax(self.shape.EWF) - np.amin(self.shape.EWF))))
+        ax1.set_title('E vs. t', pad = 15, fontsize = 20)
+        ax1.set_xlabel('t / s', labelpad = 5, fontsize = 15)
+        ax1.set_ylabel('E / V', labelpad = 5, fontsize = 15)
+
+        ax2.set_xlim(np.amin(self.instance.EPLOT) - (0.1 * (np.amax(self.instance.EPLOT) - np.amin(self.instance.EPLOT))), np.amax(self.instance.EPLOT) + (0.1 * (np.amax(self.instance.EPLOT) - np.amin(self.instance.EPLOT))))
+        ax2.set_ylim(np.amin(self.instance.i) - (0.1 * (np.amax(self.instance.i) - np.amin(self.instance.i))), np.amax(self.instance.i) + (0.1 * (np.amax(self.instance.i) - np.amin(self.instance.i))))
+        ax2.set_title('i vs. E', pad = 15, fontsize = 20)
+        ax2.set_xlabel('E / V', labelpad = 5, fontsize = 15)
+        ax2.set_ylabel('i / A', labelpad = 5, fontsize = 15)
+
+        if self.display == True:
+            plt.show()
+
+        plt.close()
+
+
+"""
+===================================================================================================
+PLOTTING DATA FROM MAIN
+===================================================================================================
+"""
+
+if __name__ == '__main__': 
+        
+     
+    '''1. MAKE THE /DATA AND /PLOT FOLDERS''' 
     cwd = os.getcwd()
 
     try:
@@ -21,7 +106,7 @@ if __name__ == '__main__':
             pass
         else: 
             raise
-    
+        
     try:
         os.makedirs(cwd + '/plots')
     except OSError as exc:
@@ -29,44 +114,21 @@ if __name__ == '__main__':
             pass
         else: 
             raise
+
+    '''2. DEFINE THE START TIME'''
+    start = time.time()  
+
+    '''3. DESCRIBE THE WAVEFORM'''
+    shape = wf.CV(Eini = 0, Eupp = 0.5, Elow = 0, dE = 0.01, sr = 0.05, ns = 1)
+    #shape = wf.CSV(Eini = 0, Eupp = 0.5, Elow = 0, dE = 0.01, sr = 0.1, ns = 1, st = 0.0001, detailed = True, sampled = True, alpha = 0.25)
     
-
-    '''SIMULATION'''
-    start = time.time()
-
-    #shape = wf.CV(Eini = 0.25, Eupp = 0.5, Elow = 0, dE = -0.005, sr = 0.1, ns = 1)
-    shape = wf.CSV(Eini = 0.5, Eupp = 0.5, Elow = 0, dE = -0.001, sr = 0.1, ns = 1, st = 0.001, detailed = True)
-
-    instance = sim.Capacitance(input = shape, Cd = 0.000050, Ru = 500)
+    '''4. RUN THE SIMULATION'''
+    instance = sim.Capacitance(input = shape, Cd = 0.0001, Ru = 750)
     
+    '''5. PLOT THE RESULTS'''
+    plotted = Plotter(shape, instance)
+
+    '''6. DEFINE THE END TIME'''
     end = time.time()
-    print(f'The simulation took {end-start} seconds to complete')
 
-
-    '''PLOT GENERATION'''
-    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12, 5))
-    left, = ax1.plot(shape.tWF, shape.EWF, linewidth = 1, linestyle = '-', color = 'blue', marker = None, label = None, visible = True)
-    if shape.detailed == True:
-        right, = ax2.plot(shape.EPLOT[shape.sp:], instance.i, linewidth = 1, linestyle = '-', color = 'red', marker = None, label = None, visible = True)
-    elif shape.detailed == False:
-        print('\n' + 'Cannot currently plot simplified CSV waveforms, try usng CV instead' + '\n')
-        sys.exit()
-    else:
-        right, = ax2.plot(shape.EPLOT[1:], instance.i, linewidth = 1, linestyle = '-', color = 'red', marker = None, label = None, visible = True)
-
-
-    '''PLOT SETTINGS'''
-    ax1.set_xlim(np.amin(shape.tWF) - (0.1 * (np.amax(shape.tWF) - np.amin(shape.tWF))), np.amax(shape.tWF) + (0.1 * (np.amax(shape.tWF) - np.amin(shape.tWF))))
-    ax1.set_ylim(np.amin(shape.EWF) - (0.1 * (np.amax(shape.EWF) - np.amin(shape.EWF))), np.amax(shape.EWF) + (0.1 * (np.amax(shape.EWF) - np.amin(shape.EWF))))
-    ax1.set_title('E vs. t', pad = 15, fontsize = 20)
-    ax1.set_xlabel('t / s', labelpad = 5, fontsize = 15)
-    ax1.set_ylabel('E / V', labelpad = 5, fontsize = 15)
-
-    ax2.set_xlim(np.amin(shape.EPLOT) - (0.1 * (np.amax(shape.EPLOT) - np.amin(shape.EPLOT))), np.amax(shape.EPLOT) + (0.1 * (np.amax(shape.EPLOT) - np.amin(shape.EPLOT))))
-    ax2.set_ylim(np.amin(instance.i) - (0.1 * (np.amax(instance.i) - np.amin(instance.i))), np.amax(instance.i) + (0.1 * (np.amax(instance.i) - np.amin(instance.i))))
-    ax2.set_title('i vs. E', pad = 15, fontsize = 20)
-    ax2.set_xlabel('E / V', labelpad = 5, fontsize = 15)
-    ax2.set_ylabel('i / A', labelpad = 5, fontsize = 15)
-
-    plt.show()
-    plt.close()
+    print(f'The simulation took {end-start} seconds to plot')
