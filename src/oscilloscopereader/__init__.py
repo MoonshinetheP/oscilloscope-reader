c884d7f8bae3a43aeb5b47b033b62aff473c100c
Steven Linfield <68279135+MoonshinetheP@users.noreply.github.com>
2023-11-03 16:19:34 +0100

    Added other files

--- src/oscilloscopereader/__init__.py
+++ src/oscilloscopereader/__init__.py
@@ -5,10 +5,12 @@ oscilloscope-reader pip version
 
 from .fileopener import *
 from .operations import *
-from .plotting import *
+from .plot import *
+from .reader import *
 from .simulations import *
+from .waveforms import *
 
 __version__ = "1.0.0"
 __author__ = 'Steven Linfield'
 
-__all__ = ['fileopener','operations','plotting','simulations']
+__all__ = ['fileopener','operations','plot','reader','simulations','waveforms']
