5745b54baba9045e2daac3b70d88fee60b10557d
Steven Linfield <68279135+MoonshinetheP@users.noreply.github.com>
2023-11-03 16:25:01 +0100

    Fixed functions and interval finding function
    Raw and MA are pretty much done, just need a quick clean up. Need to pay a little attention to how the interval is determined and used in CA

--- src/oscilloscopereader/operations.py
+++ src/oscilloscopereader/operations.py
@@ -1,87 +1,221 @@
+'''
+===================================================================================================
+Copyright (C) 2023 Steven Linfield
+
+This file is part of the oscilloscope-reader package. This package is free software: you can 
+redistribute it and/or modify it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or (at your option) any later 
+version. This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details. You should have received a copy of the GNU General 
+Public License along with oscilloscope-reader. If not, see https://www.gnu.org/licenses/
+===================================================================================================
+
+Package title:      oscilloscope-reader
+Repository:         https://github.com/MoonshinetheP/oscilloscope-reader
+Date of creation:   22/10/2022
+Main author:        Steven Linfield (MoonshinetheP)
+Collaborators:      None
+Acknowledgements:   None
+
+Filename:           operations.py
+
+===================================================================================================
+
+Description:
+
+ 
+
+===================================================================================================
+
+How to use this file:
+    
+
+
+===================================================================================================
+
+Note:
+
+
+
+===================================================================================================
+'''
+
+
+import sys
+import os
+import time
 import numpy as np
+from tkinter import filedialog
+import waveforms as wf
 import simulations as sim
+import fileopener as fo
 
-def interval(input, intervals):
-    '''This function finds the position of each transient and the real interval time between each step'''
-    '''It then uses this information to give an accurate account of the duration and position of each potential step'''
-    
-    absolute = np.abs(input)
-    
-    a = 0
-    list_of_intervals = np.array([])
+from errno import EEXIST
 
-    while a < (absolute.size - intervals + 1):
-        moving_window = absolute[a : a + intervals]
-        peak = np.argmax(moving_window)
-        list_of_intervals = np.append(list_of_intervals, int(peak) + a + 1)
-        a += intervals
-    
-    diff_of_intervals = np.diff(np.array(list_of_intervals))
+
+class Operations:
+    def __init__(self, shape, input, MA = False, window = 1000, step = 100, CS = False, alpha = 0.5):
+        self.shape = shape
+        self.input = input.i
+        self.interval = shape.interval
+        self.cf = input.cf
+        self.MA = MA
+        self.window = window
+        self.step = step
+        self.CS = CS
+        self.alpha = alpha
+
+        '''DATA TYPE ERRORS'''
+        if isinstance(self.MA, (bool)) is False:
+            print('\n' + '' + '\n')
+            sys.exit()
+        if isinstance(self.window, (int)) is False:
+            print('\n' + '' + '\n')
+            sys.exit()
+        if isinstance(self.step, (int)) is False:
+            print('\n' + '' + '\n')
+            sys.exit()
+        if isinstance(self.CS, (bool)) is False:
+            print('\n' + '' + '\n')
+            sys.exit()
+        if isinstance(self.alpha, (float, int)) is False:
+            print('\n' + '' + '\n')
+            sys.exit()
+
+        self.Intervals()
+
+        if self.MA == False and self.CS == False:
+            self.Raw()
+        if self.MA == True and self.CS == False:
+            self.MovingAverage()
+        if self.MA == False and self.CS == True:
+            self.CurrentSampling()
+        if self.MA == True and self.CS == True:
+            print('\n' + 'Both analysis methods have been selected. Please choose either one or neither in order to get the raw data' + '\n')
+            sys.exit()
+
+
+    def Intervals(self):
+        '''Finds intervals'''
         
-    minimum_interval = np.amin(list_of_intervals)
+        ix = 0
+        self.peaks = np.array([])
 
-    return (list_of_intervals, diff_of_intervals, minimum_interval)
+        while ix < (self.input.size - self.interval + 1):
+            self.peaks = np.append(self.peaks, np.argmax(np.abs(self.input[ix : ix + self.interval])) + ix + 1)
+            ix += self.interval
+        
+        self.values =np.array([])
+        for iy in self.peaks:
+            self.values = np.append(self.values, self.input[int(iy)])
 
+        iw = 0
+        self.troughs = np.array([])
+        while iw < self.peaks.size:
+            self.troughs = np.append(self.troughs, self.peaks[iw] - np.argmin(self.input[int(self.peaks[iw]) - 200: int(self.peaks[iw])]))
+            iw += 1
 
-def raw(input):
-    '''Currently returns the oscilloscope data in its raw form'''
-    raw_save = 'raw.txt'
-    with open(filepath + raw_save,'w') as file:
-        for x in input:
-            file.write(str(x) + '\n')
+        iz = 0
+        spacing = np.diff(self.values)
+        for iz in spacing:
+            if iz <= -np.abs(self.values[1]):
+                self.lv = int(self.troughs[np.where(spacing == iz)[0][0]])
+                if self.shape.dE > 0:
+                        self.EPLOT = np.concatenate((self.shape.EPLOT[self.shape.uppdp + self.shape.dp - self.lv: ], self.shape.EPLOT[:self.shape.uppdp + self.shape.dp -self.lv]))
+                elif self.shape.dE <0:
+                         self.EPLOT = np.concatenate((self.shape.EPLOT[self.shape.lowdp - self.lv: ], self.shape.EPLOT[:self.shape.lowdp - self.lv]))
+                break
 
+            elif iz >= np.abs(self.values[1]):
+                self.uv = int(self.troughs[np.where(spacing == iz)[0][0]])
+                if self.shape.dE > 0:
+                        self.EPLOT = np.concatenate((self.shape.EPLOT[self.shape.uppdp - self.uv: ], self.shape.EPLOT[:self.shape.uppdp - self.uv]))
+                elif self.shape.dE <0:
+                         self.EPLOT = np.concatenate((self.shape.EPLOT[self.shape.dp + self.shape.lowdp - self.uv: ], self.shape.EPLOT[:self.shape.dp + self.shape.lowdp - self.uv]))
+                break                         
 
-def MA(input, window = 500, step = 1):
-    '''Returns the moving average of the oscilloscope data in the form of voltage (detailed) vs. averaged current''' 
-    i = 0
-    MA_list = np.array([])
-    while i < input.size - window + 1:
-        MA_window = input[i : i + window]
-        window_average = np.sum(MA_window) / window
-        MA_list = np.append(MA_list, window_average)
-        i += step
-
-    with open(filepath + 'MA.txt','w') as file:
-        for ix in MA_list:
-            file.write(str(ix) + '\n')
+        return (self.peaks, self.troughs, self.EPLOT)
     
 
-def CA(input, intervals, alpha = 0.5):
-    '''Returns the current average of each potential step in the oscilloscope data in the form of voltage (simplified) vs. averaged current'''
+    def Raw(self):
+        '''Currently returns the oscilloscope data in its raw form'''
+        self.analysis = 'no formatting'
+            
+        self.index = self.shape.index
+        self.EPLOT = self.EPLOT
+        self.i = self.input * -self.cf
 
-    CA_data = interval(input, intervals)[0]
 
-    CA_list = np.array([])
-    if alpha > 1 or alpha < 0.005:
-        pass
-    else:
-        for x in range(0, CA_data.size):
+    def MovingAverage(self):
+        '''Returns the moving average of the oscilloscope data in the form of voltage (detailed) vs. averaged current''' 
+        self.analysis = f'a moving average using a {self.window} window and {self.step} steps'
+        a = 0
+        self.i = np.array([])
+        while a < self.input.size - self.window + 1:
+            self.i = np.append(self.i, np.average(self.input[a : a + self.window]))
+            a += self.step
+
+        self.index = self.shape.index
+        self.EPLOT = self.EPLOT[::self.step]
+        self.i = self.i * -self.cf
+        
+
+
+    def CurrentSampling(self):
+        '''Returns the current average of each potential step in the oscilloscope data in the form of voltage (simplified) vs. averaged current'''
+        self.analysis = f'current sampling using an alpha of {self.alpha}'
+
+        self.i = np.array([])
+        for ix in self.Intervals()[0]:
             try:
-                data = input[int(CA_data[x]):int(CA_data[x+1])]
+                data = self.input[int(ix) : int(ix + 1)]
             except:
-                data = input[int(CA_data[x]):] 
-            period = alpha*(data.size)                                                    
-            sampled = np.sum(data[-int(period):]) / period   
-            CA_list = np.append(CA_list, sampled)
+                data = self.input[int(ix):] 
+            period = self.alpha*(data.size)                                                    
+            sampled = np.average(data[-int(period):])   
+            self.i = np.append(self.i, sampled)
+        
+        self.index = self.shape.index
+        self.EPLOT = self.EPLOT[::self.shape.interval]
+        self.i = self.i * -self.cf
+        
     
-    CA_save = f'CA{alpha}.txt'
-    with open(filepath + CA_save,'w') as file:
-        for ix in list(CA_list):
-            file.write(str(ix) + '\n')
+    def output(self):
+        zipped = zip(self.index, self.EPLOT, self.i)
+        return zipped
 
 
 if __name__ == '__main__':
-    filepath = 'C:/Users/SLinf/Documents/GitHub/oscilloscope-reader/'
-    example = sim.Capacitance(Eini = 0.0, Eupp = 0.5, Elow = 0.0, dE = 0.002 , sr = 0.2, ns = 1, Cd = 0.000050, Ru = 500, sp = 1000)
-    staircase = example.CSV()
-    raw(staircase)
-    MA(staircase, 1000, 1)
-    CA(staircase, 1000, 0.25)
-    CA(staircase, 1000, 0.5)
-    CA(staircase, 1000, 0.75)
-    CA(staircase, 1000, 0.95)
-    '''response = interval(staircase, int(example.sp), 0.5)
-    current_save = 'current.txt'
-    with open(filepath + current_save, 'w') as file:
-        for x in response[1]:
-            file.write(str(x) + '\n')'''
+    
+    '''1. MAKE A /ANALYSIS FOLDER''' 
+    cwd = os.getcwd()
+
+    try:
+        os.makedirs(cwd + '/analysis')
+    except OSError as exc:
+        if exc.errno == EEXIST and os.path.isdir(cwd + '/analysis'):
+            pass
+        else: 
+            raise
+
+    '''2. DEFINE THE START TIME'''
+    start = time.time()  
+
+    '''3. ANALYSE THE DEFINED FILE OR SIMULATION'''
+    shape = wf.CyclicLinearVoltammetry(Eini = -0.3, Eupp = 0.8, Elow = -0.65, dE = 0.002, sr = 0.5, ns = 1, osf = 2000000)
+    
+    input = fo.Oscilloscope(filedialog.askopenfilename(), cf = 0.000012)
+    #input = sim.Capacitance(input = shape, Cd = 0.000050, Ru = 500)
+    
+    instance = Operations(shape, input, MA = True, window = 20000, step = 1000, CS = False, alpha = 0.9)
+
+    '''4. SAVE THE DATA'''
+    filepath = f'{cwd}/analysis/{time.strftime("%Y-%m-%d %H-%M-%S")} {instance.analysis}.txt'
+    with open(filepath, 'w') as file:
+        for ix, iy, iz in instance.output():
+            file.write(str(ix) + ',' + str(iy) + ',' + str(iz) + '\n')
+
+    '''5. DEFINE THE END TIME'''
+    end = time.time()
+    print(f'The oscilloscope file took {end-start} seconds to analyse')
